cf6a7e58521db9e8918c5827d45ff07de0dac69e
幻弦让叶 <1248389474@qq.com>
2023-09-14 21:28:28 +0800

    新增请求类

---
+++ app/src/main/java/com/lanier/bili/net/ServeHelper.kt
@@ -0,0 +1,36 @@
+package com.lanier.bili.net
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.lanier.lib_core.net.Serve
+import okhttp3.Request
+import okhttp3.Response
+
+/**
+ * Author: Turtledove
+ * Date  : on 2023/9/13
+ * Desc  :
+ */
+object ServeHelper {
+
+    val mapper = jacksonObjectMapper()
+
+    inline fun <reified T> fromJson(json: String): T {
+        return mapper.readValue(json)
+    }
+
+    suspend inline fun <reified T> sendRequest(
+        req: Request
+    ): T? {
+        val response = request(req)
+        val json = response.body?.string()
+        if (!json.isNullOrEmpty()) {
+            return fromJson(json)
+        }
+        return null
+    }
+
+    suspend fun request(req: Request): Response {
+        return Serve.sendRequest(req)
+    }
+}
